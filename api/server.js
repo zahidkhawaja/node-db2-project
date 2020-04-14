1fac9e52ec70c09b756787b427f34f98c6b0e988
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-14 15:00:56 -0400

    Add endpoints and Knex migrations

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const carsRouter = require("../cars/cars-router.js");
+
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/cars", carsRouter);
 
 server.get("/", (req, res) => {
     res.status(200).json({ message: "API working"})
